da613d12a075419b4ea31d04418b4ecc6ea7dd0e
rianfaisalm <rian.faisalm@gmail.com>
2023-10-02 17:15:04 +0700

    update

--- src/pages/client.js
+++ src/pages/client.js
@@ -1,47 +1,13 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
-import Details from '@/components/client/details';
+import Hero from '@/components/about/hero';
 
-const Client = () => {
-  const [data, setData] = useState({});
-  const [isLoading, setLoading] = useState(true);
-
-  useEffect(() => {
-    axios
-      .get('https://jsonplaceholder.typicode.com/users')
-      .then((res) => {
-        setData(res.data);
-        setLoading(false);
-      })
-      .catch((error) => console.log(error));
-  }, []);
-  if (isLoading)
-    return (
-      <div className="max-w-screen-xl mx-auto h-screen bg-gray-400 flex items-center justify-center">
-        <h1 className="font-bold text-2xl">Loading</h1>
-      </div>
-    );
-  if (!data)
-    return (
-      <div className="max-w-screen-xl mx-auto h-screen bg-gray-400 flex items-center justify-center">
-        <h1 className="font-bold text-2xl">No data</h1>
-      </div>
-    );
-  console.log('data', data);
+const About = () => {
   return (
-    <div className="max-w-screen-xl mx-auto h-screen bg-gray-400">
-      <ul className="flex flex-col gap-4">
-        {data?.map((item, index) => (
-          <p key={index} className="text-bold text-base">
-            {item?.name}
-          </p>
-        ))}
-      </ul>
-      {/*  halaman fetch dari components */}
-      <Details />
+    <div className="h-screen bg-slate-200">
+      <Hero />
     </div>
   );
 };
 
-export default Client;
+export default About;
